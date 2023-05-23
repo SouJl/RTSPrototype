ab31058cda6624f85fd426eba63de1eebb763608
Александр Мельников <meln.alex97@gmail.com>
2023-05-23 23:35:26 +0700

    Implement Outline to selectable objects

---
+++ Assets/RTSPrototype/Scripts/UserControlSystem/UI/Presenter/SelectedOutlinePresenter.cs
@@ -0,0 +1,25 @@
+﻿using RTSPrototype.Abstractions;
+using RTSPrototype.UIModel;
+using UnityEngine;
+
+namespace RTSPrototype.UIPresenter
+{
+    public class SelectedOutlinePresenter :  MonoBehaviour
+    {
+        [Header("Outline")]
+        [SerializeField] private Outline _outline;
+        [Header("Selected Value")]
+        [SerializeField] private SelectedValue _selectedObject;
+
+        private void Start()
+        {
+            _selectedObject.OnSelected += OnSelected;
+            OnSelected(_selectedObject.CurrentValue);
+        }
+
+        private void OnSelected(ISelectable selectable)
+        {
+            _outline.enabled = selectable != null;
+        }
+    }
+}
