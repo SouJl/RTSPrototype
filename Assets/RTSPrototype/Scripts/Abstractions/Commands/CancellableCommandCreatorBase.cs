f231880d9383e6b24cefc73fc477fb71c5a53d40
SouJl <meln.alex97@gmail.com>
2023-05-30 15:41:40 +0700

    Added Asycn logic
    Create base interfaces and classes for further async/await realization

---
+++ Assets/RTSPrototype/Scripts/Abstractions/Commands/CancellableCommandCreatorBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using RTSPrototype.Abstractions.Commands;
+using RTSPrototype.Utils;
+using Zenject;
+
+namespace RTSPrototype.Abstractions
+{
+    public abstract class CancellableCommandCreatorBase<TCommand, TArgument> : CommandCreatorBase<TCommand> where TCommand : ICommand
+    {
+        [Inject] private AssetsContext _context;
+        [Inject] private IAwaitable<TArgument> _awaitableArgument;
+
+        private CancellationTokenSource _ctSource;
+        
+        protected override sealed async void classSpecificCommandCreation(Action<TCommand> creationCallback)
+        {
+            _ctSource = new CancellationTokenSource();
+            try
+            {
+                var argument = await _awaitableArgument.WithCancellation(_ctSource.Token);
+                creationCallback?.Invoke(_context.Inject(CreateCommand(argument)));
+            }
+            catch 
+            {
+
+            }
+        }
+
+        protected abstract TCommand CreateCommand(TArgument argument);
+
+        public override void ProcessCancel()
+        {
+            base.ProcessCancel();
+            if (_ctSource != null)
+            {
+                _ctSource.Cancel();
+                _ctSource.Dispose();
+                _ctSource = null;
+            }
+        }
+    }
+}
