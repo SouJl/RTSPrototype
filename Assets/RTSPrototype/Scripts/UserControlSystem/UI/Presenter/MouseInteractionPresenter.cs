f45d00eedf4fc68cbe2695fa27fbb06d15830b30
SouJl <meln.alex97@gmail.com>
2023-06-05 10:22:58 +0700

    Change to UniRx in MousePresenter

--- Assets/RTSPrototype/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
+++ Assets/RTSPrototype/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using RTSPrototype.Abstractions;
 using UnityEngine.EventSystems;
 using Zenject;
+using UniRx;
 
 namespace RTSPrototype.UIPresenter 
 {
@@ -17,50 +18,54 @@ namespace RTSPrototype.UIPresenter
         [Inject] private IRTSValue<Vector3> _groundClicksRMB;
         [Inject] private IRTSValue<IAttackable> _attackablesRMB;
 
-
         private Plane _groundPlane;
 
         private void Start()
         {
             _groundPlane = new Plane(_groundTransform.up, 0);
+
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetMouseButtonUp(0) && !EventSystem.current.IsPointerOverGameObject())
+                .Select(_ => _camera.ScreenPointToRay(Input.mousePosition))
+                .Select(ray => Physics.RaycastAll(ray))
+                .Subscribe(hits =>
+                {
+                    OnLeftMouseClick(hits);
+                });
+
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetMouseButtonUp(1) && !EventSystem.current.IsPointerOverGameObject())
+                .Select(_ => _camera.ScreenPointToRay(Input.mousePosition))
+                .Select(ray => (ray, Physics.RaycastAll(ray)))
+                .Subscribe(rayData =>
+                {
+                    var (ray, hits) = rayData;
+                    OnRightMouseClick(ray, hits);
+                });
+
         }
 
-        private void Update()
+        private void OnLeftMouseClick(RaycastHit[] hits)
         {
-            if (!Input.GetMouseButtonUp(0) && !Input.GetMouseButton(1))
+            if (CheckForHit<ISelectable>(hits, out var selectable))
             {
-                return;
+                _selectedLMB.SetValue(selectable);
             }
-
-            if (EventSystem.current.IsPointerOverGameObject())
-                return;
-
-            var ray = _camera.ScreenPointToRay(Input.mousePosition);
-            var hits = Physics.RaycastAll(ray);
-
-            if (Input.GetMouseButtonUp(0))
+            else
             {
-                if (CheckForHit<ISelectable>(hits, out var selectable))
-                {
-                    _selectedLMB.SetValue(selectable);
-                }
-                else 
-                {
-                    _selectedLMB.SetValue(null);
-                }
-            }            
-            else if(Input.GetMouseButton(1))
-            {
-                if (CheckForHit<IAttackable>(hits, out var attackable))
-                {
-                    _attackablesRMB.SetValue(attackable);
-                }
-                else if (_groundPlane.Raycast(ray, out var enter)) 
-                {
-                    _groundClicksRMB.SetValue(ray.origin + ray.direction * enter);
-                }
+                _selectedLMB.SetValue(null);
             }
-
+        }
+        private void OnRightMouseClick(Ray ray, RaycastHit[] hits)
+        {
+            if (CheckForHit<IAttackable>(hits, out var attackable))
+            {
+                _attackablesRMB.SetValue(attackable);
+            }
+            else if (_groundPlane.Raycast(ray, out var enter))
+            {
+                _groundClicksRMB.SetValue(ray.origin + ray.direction * enter);
+            }
         }
 
         private bool CheckForHit<THit>(RaycastHit[] hits, out THit result) where THit :class
