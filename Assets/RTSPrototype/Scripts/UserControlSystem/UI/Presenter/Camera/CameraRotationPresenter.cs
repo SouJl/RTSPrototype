5c7c50a18041b3279ab0f88ca48299445383a203
SouJl <meln.alex97@gmail.com>
2023-06-08 13:54:35 +0700

    Create Camera motion and rotate presenters

---
+++ Assets/RTSPrototype/Scripts/UserControlSystem/UI/Presenter/Camera/CameraRotationPresenter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace RTSPrototype.UIPresenter
+{
+    public class CameraRotationPresenter : MonoBehaviour
+    {
+		[SerializeField] private float _speed = 15f;
+		[SerializeField] private float _smoothing = 5f;
+
+		private float _targetAngle;
+		private float _currentAngle;
+
+		private void Awake()
+		{
+			_targetAngle = transform.eulerAngles.y;
+			_currentAngle = _targetAngle;
+		}
+		private void Update()
+		{
+			HandleInput();
+			Rotate();
+		}
+		private void HandleInput()
+		{
+			if (!Input.GetMouseButton(1)) return;
+			_targetAngle += Input.GetAxisRaw("Mouse X") * _speed;
+		}
+
+		private void Rotate()
+		{
+			_currentAngle = Mathf.Lerp(_currentAngle, _targetAngle, Time.deltaTime * _smoothing);
+			transform.rotation = Quaternion.AngleAxis(_currentAngle, Vector3.up);
+		}
+	}
+}
