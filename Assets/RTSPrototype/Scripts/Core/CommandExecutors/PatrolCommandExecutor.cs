7d0138109fbeb8cc2530f9327a8f4aa53edd45cd
SouJl <meln.alex97@gmail.com>
2023-06-15 14:39:34 +0700

    Implement patrol logic

--- Assets/RTSPrototype/Scripts/Core/CommandExecutors/PatrolCommandExecutor.cs
+++ Assets/RTSPrototype/Scripts/Core/CommandExecutors/PatrolCommandExecutor.cs
@@ -1,18 +1,117 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using RTSPrototype.Abstractions;
 using RTSPrototype.Abstractions.Commands;
 using RTSPrototype.Abstractions.Commands.CommandInterfaces;
+using RTSPrototype.Core.Operations;
 using UnityEngine;
+using UnityEngine.AI;
+using Zenject;
+using UniRx;
+using RTSPrototype.Utils;
+using System.Threading;
 
 namespace RTSPrototype.Core.CommandExecutors
 {
     public class PatrolCommandExecutor: CommandExecutorBase<IPatrolCommand>
     {
-        public override async Task ExecuteSpecificCommand(IPatrolCommand command) => 
-            ExecutePatrol(command);
+        [SerializeField] private AnimatorHandler _animator;
+        [SerializeField] private MovementStopOperation _movementStop;
+        [SerializeField] private StopCommandExecutor _stopCommandExecutor;
 
-        private void ExecutePatrol(IPatrolCommand command)
+        [Inject] private IPauseHandler _pauseHandler;
+
+        private NavMeshAgent _curentAgent;
+        private IDisposable _pauseEvent;
+
+        private void Start()
         {
-            Debug.Log($"Execute Patrol by {name} from {command.StartPosition} to {command.EndPosition}");
+            _pauseEvent = _pauseHandler.IsPaused.Subscribe(OnPause);
+        }
+
+        private void OnDestroy()
+        {
+            _pauseEvent.Dispose();
+        }
+
+        private void Awake()
+        {
+            _curentAgent = GetComponent<NavMeshAgent>();
+
+            if (TryGetComponent<MovementStopOperation>(out var movementStop))
+            {
+                _movementStop ??= movementStop;
+            }
+            else
+            {
+                Debug.Log($"Cant't find UnitMovementStop on {name}");
+            }
+
+            if (TryGetComponent<StopCommandExecutor>(out var stopCommandExecutor))
+            {
+                _stopCommandExecutor ??= stopCommandExecutor;
+            }
+            else
+            {
+                Debug.Log($"Cant't find StopCommandExecutor on {name}");
+            }
+        }
+
+        public override async Task ExecuteSpecificCommand(IPatrolCommand command) => 
+            await ExecutePatrol(command);
+
+        private async Task ExecutePatrol(IPatrolCommand command)
+        {
+            var startPoint = command.StartPosition;
+            var endPoint = command.EndPosition;
+
+            while (true)
+            {
+                _curentAgent.destination = endPoint;
+                
+                Vector3 targetDirection = endPoint - transform.position;
+                transform.rotation = Quaternion.LookRotation(targetDirection);
+
+                _animator.ChangeState(AnimationType.Walk);
+                _stopCommandExecutor.CancellationTokenSource = new CancellationTokenSource();
+                try
+                {
+                    await _movementStop
+                        .RunWithCancellation(
+                        _stopCommandExecutor
+                        .CancellationTokenSource
+                        .Token);
+                }
+                catch
+                {
+                    _curentAgent.isStopped = true;
+                    _curentAgent.ResetPath();
+                    break;
+                }
+                var temp = startPoint;
+                startPoint = endPoint;
+                endPoint = temp;
+                
+                try
+                {
+                    _animator.ChangeState(AnimationType.Idle);
+                    await Task.Delay(1000, _stopCommandExecutor.CancellationTokenSource.Token);
+                }
+                catch
+                {
+                    _curentAgent.isStopped = true;
+                    _curentAgent.ResetPath();
+                    break;
+                }
+            }
+
+            _stopCommandExecutor.CancellationTokenSource = null;
+            _animator.ChangeState(AnimationType.Idle);
+        }
+
+        private void OnPause(bool pasue)
+        {
+            _curentAgent.isStopped = pasue;
         }
     }
 }
